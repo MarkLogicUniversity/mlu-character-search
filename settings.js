c597f8e22db917e834b193b604356e538b3f5060
Tamas Piros <me@tamas.io>
2015-03-15 08:08:21 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,7 +1,7 @@
 /**
  * These are the settings to connect to the MarkLogic backend
  *
- * First please make sure that you run the curl statement from the ml-rest folder. That curl statement sets up an application inside MarkLogic that will run on port 5001. It will also automatically create a database.
+ * First please make sure that you run the curl statement from the ml-rest folder. That curl statement sets up an application inside MarkLogic that will run on port 5002. It will also automatically create a database.
  */
 var connection = {
 	host: 'localhost',
